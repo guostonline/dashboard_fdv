38639b658c259062a041682b93350e5808667af4
chakib <guostonline@gmail.com>
2023-09-13 12:51:15 +0100

    beta2

--- app.py
+++ app.py
@@ -8,26 +8,46 @@ from src.suivi import Suivi
 
 st.set_page_config(page_title="Rapport FDV", page_icon=":bar_chart:", layout="wide")
 get_day_work = 24
-total_days_month, days_worked = 24, 1
+
+if "total_days_month" not in st.session_state:
+    st.session_state.total_days_month = (24,)
+if "days_worked" not in st.session_state:
+    st.session_state.days_worked = (1,)
+
 day_work = 1
 real_days_rest = 1
+
+
+if "load_state" not in st.session_state:
+    st.session_state.load_state = True
+
+
+
+
+
+
 uploaded_file = st.sidebar.file_uploader(
     "Choisir un fichier excel.",
     type="xlsx",
 )
 
-if uploaded_file is not None:
+
+if uploaded_file is not None and st.session_state.load_state:
     sheet = Excel(uploaded_file)
     sheet.fix_sheet()
     sheet.get_day_work()
-    days_worked, total_days_month = sheet.get_day_work()
-suivi = Suivi()
-
-st.sidebar.text(f"Days works: {days_worked}/{total_days_month}")
+    (
+        st.session_state.days_worked,
+        st.session_state.total_days_month,
+    ) = sheet.get_day_work()
+    st.session_state.load_state = False
 real_days_rest = st.sidebar.number_input("Real days rest", min_value=1, max_value=26)
-suivi.real_days_rest = real_days_rest
-suivi.days_towork = total_days_month
-suivi.worked_days = days_worked
+
+
+st.sidebar.text(
+    f"Days works: {st.session_state.days_worked}/{st.session_state.total_days_month}"
+)
+
 
 categories = st.sidebar.selectbox(
     "Categories", [categorie.value for categorie in Categorie]
@@ -41,11 +61,15 @@ select_vendeur = st.sidebar.multiselect(
 select_famille = st.sidebar.multiselect(
     "Select Famille",
     [famille.value for famille in Famille],
-    default='C.A (ht)'
+    default="C.A (ht)",
+    
 )
 options_extra = st.sidebar.selectbox("Extra", [hScore.value for hScore in Extra])
+suivi = Suivi(
+    st.session_state.total_days_month, st.session_state.days_worked, real_days_rest
+)
 df = suivi.filter_vendeur_famille(select_vendeur, select_famille)
-
+print(st.session_state.total_days_month, st.session_state.days_worked, real_days_rest)
 # df_mod = df.query("Famille==@select_famille & Vendeur==@select_vendeur")
 
 st.dataframe(df)
--- src/suivi.py
+++ src/suivi.py
@@ -4,13 +4,15 @@ from src.myEnum import Extra
 
 
 class Suivi:
-    days_towork=0
-    worked_days=0
-    real_days_rest=0
-    def __init__(self,) -> None:
-        
+    def __init__(self, days_towork, worked_days, real_days_rest) -> None:
+        self.days_towork = days_towork
+        self.worked_days = worked_days
+        self.real_days_rest = real_days_rest
+
         self.df = pd.read_excel("excel/finale.xlsx")
-        list_of_string_H: list = self.df[self.df["H"].apply(lambda x: isinstance(x, str))]
+        list_of_string_H: list = self.df[
+            self.df["H"].apply(lambda x: isinstance(x, str))
+        ]
 
         for i in list_of_string_H.index:
             self.df.loc[i, "H"] = 0
@@ -23,23 +25,37 @@ class Suivi:
                 "Historique 2022": "int",
             }
         )
-       
-        
-        #self.df["OBJ ttc"] = self.df.OBJ.apply(lambda x: (x * (self.df["OBJ"]*(days_towork/worked_days) ))* 1.2)
-        self.df["REAL"]=self.df["REAL"]+self.df["EnCours"]
-        self.df["Percent"]=self.df["REAL"]/self.df["OBJ"]-1
+
+        # self.df["OBJ ttc"] = self.df.OBJ.apply(lambda x: (x * (self.df["OBJ"]*(days_towork/worked_days) ))* 1.2)
+        self.df["REAL"] = self.df["REAL"] + self.df["EnCours"]
+        self.df["Percent"] = self.df["REAL"] / self.df["OBJ"] - 1
+
         self.df.loc[:, "Percent"] = self.df["Percent"].map("{:.1%}".format)
 
         self.df.loc[:, "H"] = self.df["H"].map("{:.1%}".format)
-        self.df.replace(0,1,inplace=True)
-        self.df["OBJ ttc"]=round(self.df["OBJ"]*self.days_towork/self.worked_days)*1.2
-        self.df['RAF']=round((self.df['OBJ ttc']-(self.df["REAL"]*1.2))/self.real_days_rest)
-        
+        self.df.replace(0, 1, inplace=True)
+        self.df["OBJ ttc"] = round(
+            (self.df["OBJ"] * self.days_towork / self.worked_days) * 1.2
+        )
+
+        self.df["RAF"] = (
+            self.df["OBJ ttc"] - (self.df["REAL"] * 1.2)
+        ) / self.real_days_rest
+        # self.df["Percent"]=self.df["Percent"].pct_change().style.highlight_max(color="lightgreen")
+        self.df = self.df.astype(
+            {
+                "OBJ ttc": "int",
+                "RAF": "int",
+            }
+        )
+
     def filter_vendeur_famille(self, vendeur: str, famille: str):
-        
         df_mod = self.df.query("Famille==@famille & Vendeur==@vendeur")
-        df_mod=df_mod.style.highlight_max(subset=["REAL"]).highlight_min(color="red" ,subset=["REAL"])
-       
+        df_mod = (
+            df_mod.style.background_gradient(subset=["REAL"])
+            .highlight_max(subset=["Percent"])
+            .highlight_min(subset=["Percent", "REAL"], color="orange")
+        )
 
         return df_mod
 
@@ -47,6 +63,3 @@ class Suivi:
     def get_all_vendeurs(self):
         all_fdv = self.df["Vendeur"].unique()
         return all_fdv
-    
-    
-        
